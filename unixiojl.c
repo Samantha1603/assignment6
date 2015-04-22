2168fe2795908e129edb7a19a0f3ae1018f91750
lee <leejohng1@gmail.com>
2015-04-21 19:47:23 -0700

    progress commit group meeting

--- unixiojl.c
+++ unixiojl.c
@@ -9,25 +9,13 @@
 #include <signal.h>
 #include <assert.h>
 
-#define READ_END_OF_PIPE 0
-#define WRITE_END_OF_PIPE 1
+#define READ_END 0
+#define WRITE_END 1
+#define BUFFER_SIZE 64
+#define DURATION 30
 
-int main(int argc, char *argv[]){
-  // create 5 pipes
-  pid_t pid;
-  int pipe_fd1[2];
-  pid_t pid;
-  int pipe_fd2[2];
-
-  if(pipe(pipe_fd1) == -1){
-    fprintf(stderr, "pipe() failed");
-  }
-  if(pipe(pipe_fd2) == -1){
-    fprintf(stderr, "pipe() failed");
-  }
-  
-  
-  // create 5 child processes, start with 1 for now.
+int timeup = 0;
+  // create 5 child processes, start with 2 for now.
   // each process may write to its pipe
   // each message will have its own timestamp
   // sleep for random 0-2 seconds
@@ -39,6 +27,62 @@ int main(int argc, char *argv[]){
   // write out the line to output.txt.
 
   //terminate parent after all child processes are completed.
+
+int main(int argc, char *argv[]){
+  // create 5 pipes
+  // pid_t pid1, pid2, pid3, pid4, pid5;
+  pid_t pid1;
+  pid_t pid2;
+
+  int fd1[2];
+  int fd2[2];
+  int result, nread;
+  fd_set inputs, inputfds;  // sets of file descriptors
+
+  FD_ZERO(&inputs);
+  FD_SET(fd1[WRITE_END], &inputs);
+  FD_SET(fd2[WRITE_END], &inputs);
+
+  char write_msg[BUFFER_SIZE];
+  char read_msg[BUFFER_SIZE];
+
+
+  if (pipe(fd1) == -1) {
+    fprintf(stderr,"pipe() failed");
+    return 1;
+  }
+  if (pipe(fd2) == -1) {
+    fprintf(stderr,"pipe() failed");
+    return 1;
+  }
+  pid1 = fork();
+  if (pid1 > 0) {  // this is the parent
+    // Close the unused READ end of the pipe.
+    close(fd1[WRITE_END]);
+    // loop while not finished
+    // Read from READ end of the pipe.
+    result = select(FD_SETSIZE, &inputfds, 
+		    (fd_set *) 0, (fd_set *) 0, &timeout);
+    //check which ones are here and print message
+    //  within each check, when the message is received 
+    //  generate timestamp and add it to message
+    
+    //output to file
+    printf("Parent: Read '%s' from the pipe.\n", read_msg);
+  }
+  else if (pid == 0){
+    close(fd1[READ_END]);
+    // loop while not finished
+    // generate a random value for sleeping
+    // generate write message
+    // get timestamp
+    write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
+  }
+
+  // join all threads to parent.
+  // kill(pid_t pid, SIGKILL)
+
+  
 
   return 0;
 }
