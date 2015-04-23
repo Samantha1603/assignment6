59d321f42323ef2ff9e78b2afd54c32962f5f252
lee <leejohng1@gmail.com>
2015-04-22 19:21:38 -0700

    need to fix zombie processes

--- unixiojl.c
+++ unixiojl.c
@@ -67,28 +67,34 @@ void SIGALRM_handler(int signo)
 
 int main(int argc, char *argv[]){
   
-  pid_t pid1; // create process ids for the forks
+  pid_t pid1,pid2; // create process ids for the forks
 
-  int fd1[2];
+  int fd1[2], fd2[2];
   int result, nread;
   fd_set inputs, inputfds;  // sets of file descriptors
 
   char *write_msg1=(char *)calloc(BUFFER_SIZE, sizeof(char));
-  char *read_msg1=(char *)calloc(BUFFER_SIZE, sizeof(char));
-  struct timeval timeout;
-  timeout.tv_sec = DURATION;
+  char *write_msg2=(char *)calloc(BUFFER_SIZE, sizeof(char));
+  char *read_msg=(char *)calloc(BUFFER_SIZE, sizeof(char));
+
   srand((unsigned) time(0));
 
   if (pipe(fd1) == -1) {
     fprintf(stderr,"pipe() failed\n");
     return 1;
   }
+  if (pipe(fd2) == -1) {
+    fprintf(stderr,"pipe() failed\n");
+    return 1;
+  }
+
   FD_ZERO(&inputs);
   FD_SET(fd1[READ_END], &inputs);
+  FD_SET(fd2[READ_END], &inputs);
   signal(SIGALRM, SIGALRM_handler);
   time(&startTime);
 
-  inputfds = inputs;
+
 
   printf("Forking now\n");
   pid1 = fork();
@@ -96,56 +102,87 @@ int main(int argc, char *argv[]){
     sessionTimer.it_value.tv_sec = DURATION;
     setitimer(ITIMER_REAL, &sessionTimer, NULL);
     close(fd1[WRITE_END]); // Close the unused READ end of the pipe.
-    // loop while not finished
-    printf("waiting for results\n");
-    result = select(FD_SETSIZE, &inputfds, 
-		    (fd_set *) 0, (fd_set *) 0, (struct timeval *) &timeout);
-    printf("number of results are %d\n", result);
-    switch(result){
-    case 0: 
-      printf("timedout\n");
-      break;
 
-    case -1:
-      perror("select\n");
-      exit(1);
-      break;
+    while(!timeup){ // loop while not finished
+      inputfds = inputs;      
+      printf("waiting for results\n");
+      result = select(FD_SETSIZE, &inputfds, 
+		      NULL, NULL, NULL);
+      //printf("number of results are %d\n", result);
+      switch(result){
+      case 0: 
+	printf("timedout\n");
+	break;
 
-    default:
-      printf("in default case\n");
-      if(FD_ISSET(fd1[READ_END], &inputfds)){ // file descriptor 1
-	printf("FD_ISSET is true\n");
-	ioctl(fd1[READ_END], FIONREAD, &nread);
-	printf("found something in inputfds set\n");
-	if(nread==0){
-	  printf("Nothing to read\n");
-	  exit(0);
+      case -1:
+	perror("select\n");
+	exit(1);
+	break;
+
+      default:
+	printf("in default case\n");
+
+	if(FD_ISSET(fd1[READ_END], &inputfds)){ // file descriptor 1
+	  ioctl(fd1[READ_END], FIONREAD, &nread);
+	  if(nread==0){
+	    printf("Nothing to read\n");
+	    exit(0);
+	  }
+	  nread = read(fd1[READ_END], read_msg, nread);
+	  read_msg = (char *) insertTimestamp(read_msg);
+	  printf("Parent: Read '%s' from the pipe.\n", read_msg);
 	}
+	if(FD_ISSET(fd2[READ_END], &inputfds)){ // file descriptor 2
+	  ioctl(fd2[READ_END], FIONREAD, &nread);
+	  if(nread==0){
+	    printf("Nothing to read\n");
+	    exit(0);
+	  }
+	  nread = read(fd2[READ_END], read_msg, nread);
+	  read_msg = (char *) insertTimestamp(read_msg);  
+	  printf("Parent: Read '%s' from the pipe.\n", read_msg);
+	}
+      }// end switch-case stmt
+      //output to file
 
-	nread = read(fd1[READ_END], read_msg1, nread);
-	read_msg1 = (char *) insertTimestamp(read_msg1);  
-	printf("number of bytes read %d\n", nread);
-      }
-      // if statement for file descriptor 2
-      // and so on
     }
-    //output to file
-    printf("Parent: Read '%s' from the pipe.\n", read_msg1);
   }
   else{
-    printf("in child process %d\n", pid1);
-    close(fd1[READ_END]);
-    // loop while not finished
-    sleep(rand() % 3);
-    write_msg1 = "Message from child 1\n";
-    write_msg1 = (char *) insertTimestamp(write_msg1);
-    printf("wrote my message\n");
-    int nwrote;
-    nwrote = write(fd1[WRITE_END], write_msg1, strlen(write_msg1)+1);
-    printf("sent my message with %d bytes\n", nwrote);
+    // fork again
+    pid2 = fork();
+    if(pid2>0){ // child - parent
+      while(!timeup){
+	printf("in child process %d\n", pid1);
+	close(fd1[READ_END]);
+	// loop while not finished
+	sleep(rand() % 3);
+	write_msg1 = "Message from child 1";
+	write_msg1 = (char *) insertTimestamp(write_msg1);
+	int nwrote;
+	nwrote = write(fd1[WRITE_END], write_msg1, strlen(write_msg1)+1);
+	printf("sent my message with %d bytes\n", nwrote);
+      }
+
+    }
+    else{ // child - child
+      
+      while(!timeup){
+	printf("in child process %d\n", pid2);
+	close(fd1[READ_END]);
+	// loop while not finished
+	sleep(rand() % 3);
+	write_msg2 = "Message from child 2";
+	write_msg2 = (char *) insertTimestamp(write_msg2);
+	int nwrote;
+	nwrote = write(fd2[WRITE_END], write_msg2, strlen(write_msg2)+1);
+	printf("sent my message with %d bytes\n", nwrote);
+      }
+    }
   }
 
   // join all threads to parent.
+  kill(pid1, SIGKILL);
+  kill(pid2, SIGKILL);
   // kill(pid_t pid, SIGKILL)
   return 0;
 }
